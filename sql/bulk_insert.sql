bcdc746a23cdf9ca3bbced4d76d9c26d7530b801
Debbhie <zeechat143@gmail.com>
2025-08-01 18:27:41 +0100

    Update bulk_insert.sql

--- sql/bulk_insert.sql
+++ sql/bulk_insert.sql
@@ -1,1 +1,13 @@
+-- This scripts loads the UBA bank statement csv file into sql table
+-- Make sure you using the right path in your laptop
+
+BULK INSERT Debbhie_Uba
+FROM 'C:\Users\DEB OLUFUNSO\Debbhie_Ubastatement.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    CODEPAGE = '65001',
+    TABLOCK
+);
 
